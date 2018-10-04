084375547a258177060d71fd4049e543260b1958
vargo <jvargo@gmail.com>
2018-10-04 11:31:36 -0700

    periodic update

--- processHMS_archivesBG.R
+++ processHMS_archivesBG.R
@@ -39,6 +39,7 @@ HMSgunzip <- function(gzFiles){
 }
 
 
+
 intersectHMSarchive <- function(HMSday){
   
   year <- substr(HMSday, 1,4)
@@ -71,7 +72,7 @@ intersectHMSarchive <- function(HMSday){
              )
     
     
-    SmkTractDayInt <- st_intersection(st_make_valid(Smk), foo) %>%
+    SmkTractDayInt <- st_intersection(st_make_valid(Smk), popCentroidsBG) %>%
       select(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION)
     
     st_geometry(SmkTractDayInt) <- NULL
@@ -81,32 +82,8 @@ intersectHMSarchive <- function(HMSday){
       group_by(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION) %>%
       summarise(yn = mean(yn)) %>%
       spread(key = smoke,value = yn) %>%
-      saveRDS(paste0("~/data/US_BG/US_BG",HMSday,".RDS"))
+      fwrite(paste0("~/data/US_BG/CSV/US_BG",HMSday,".csv"))
     
-    # SmkTractDayInt <- st_intersection(st_make_valid(Smk),WAtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkTractDayInt) <- NULL
-    # 
-    # SmkTractDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/WA_tracts/WA_WF_tracts_data_",HMSday,".RDS"))
-    # 
-    # 
-    # SmkCountyDayInt <- st_intersection(st_make_valid(Smk),ORtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkCountyDayInt) <- NULL
-    # 
-    # SmkCountyDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/OR_tracts/OR_WF_tracts_data_",HMSday,".RDS"))
     # clear temp folder
     
   unlink(paste0(tempdir(),'/*'))
@@ -125,7 +102,9 @@ unlink(paste0(tempdir(),'/*'))
 
 # list.files(tempdir())
 # 
-# HMSday <- dateListrecent[1]
+ # HMSday <- dateListrecent[1]
+
+
 
 intersectHMSrecent <- function(HMSday){
   
@@ -144,10 +123,6 @@ intersectHMSrecent <- function(HMSday){
                     paste0("http://satepsanone.nesdis.noaa.gov/pub/FIRE/HMS/GIS/ARCHIVE/hms_smoke",HMSday,".shx")
     )
 
-    
-    
-   
-    
     shpNames <- list("HMSGIS.dbf","HMSGIS.shp","HMSGIS.shx")
     
     GISfiles <- mapply(download.file, shpFiles, shpNames, MoreArgs = list(method = "auto", mode = "wb"))
@@ -163,7 +138,7 @@ intersectHMSrecent <- function(HMSday){
       )
     
     
-    SmkTractDayInt <- st_intersection(st_make_valid(Smk), foo) %>%
+    SmkTractDayInt <- st_intersection(st_make_valid(Smk), popCentroidsBG) %>%
       select(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION)
     
     st_geometry(SmkTractDayInt) <- NULL
@@ -173,57 +148,7 @@ intersectHMSrecent <- function(HMSday){
       group_by(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION) %>%
       summarise(yn = mean(yn)) %>%
       spread(key = smoke,value = yn) %>%
-      saveRDS(paste0("~/data/US_BG/US_BG",HMSday,".RDS"))
-    
-    # SmkTractDayInt <- st_intersection(st_make_valid(Smk),CAtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkTractDayInt) <- NULL
-    # 
-    # SmkTractDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/CA_tracts/CA_WF_tracts_data_",HMSday,".RDS"))
-    # 
-    # 
-    # SmkCountyDayInt <- st_intersection(st_make_valid(Smk),UScountyFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkCountyDayInt) <- NULL
-    # 
-    # SmkCountyDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/US_counties/US_WF_counties_data_",HMSday,".RDS"))
-    # 
-    # SmkTractDayInt <- st_intersection(st_make_valid(Smk), WAtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkTractDayInt) <- NULL
-    # 
-    # SmkTractDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/WA_tracts/WA_WF_tracts_data_",HMSday,".RDS"))
-    # 
-    # 
-    # SmkCountyDayInt <- st_intersection(st_make_valid(Smk), ORtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkCountyDayInt) <- NULL
-    # 
-    # SmkCountyDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/OR_tracts/OR_WF_tracts_data_",HMSday,".RDS"))
+      fwrite(paste0("~/data/US_BG/CSV/US_BG",HMSday,".csv"))
     
     # clear temp folder
     
@@ -237,7 +162,6 @@ intersectHMSrecent <- function(HMSday){
   
 }
 
-HMSday <- "20150120"
 
 intersectHMSdirect <- function(HMSday){
   
@@ -257,7 +181,7 @@ intersectHMSdirect <- function(HMSday){
       )
     
     
-    SmkTractDayInt <- st_intersection(st_make_valid(Smk), foo) %>%
+    SmkTractDayInt <- st_intersection(st_make_valid(Smk), popCentroidsBG) %>%
       select(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION)
 
     st_geometry(SmkTractDayInt) <- NULL
@@ -267,46 +191,8 @@ intersectHMSdirect <- function(HMSday){
       group_by(date, smoke, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE, POPULATION) %>%
       summarise(yn = mean(yn)) %>%
       spread(key = smoke,value = yn) %>%
-      saveRDS(paste0("~/data/US_BG/US_BG",HMSday,".RDS"))
+      fwrite(paste0("~/data/US_BG/CSV/US_BG",HMSday,".csv"))
     
-    # 
-    # SmkCountyDayInt <- st_intersection(st_make_valid(Smk), UScountyFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkCountyDayInt) <- NULL
-    # 
-    # SmkCountyDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/US_counties/US_WF_counties_data_",HMSday,".RDS"))
-    # 
-    # 
-    # SmkTractDayInt <- st_intersection(st_make_valid(Smk), WAtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkTractDayInt) <- NULL
-    # 
-    # SmkTractDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/WA_tracts/WA_WF_tracts_data_",HMSday,".RDS"))
-    # 
-    # 
-    # SmkCountyDayInt <- st_intersection(st_make_valid(Smk), ORtractFile) %>%
-    #   select(date, smoke, geoIDer)
-    # 
-    # st_geometry(SmkCountyDayInt) <- NULL
-    # 
-    # SmkCountyDayInt  %>%
-    #   mutate(yn = 1) %>%
-    #   group_by(date, smoke, geoIDer) %>%
-    #   summarise(yn = mean(yn)) %>%
-    #   spread(key = smoke,value = yn) %>%
-    #   saveRDS(paste0("~/data/OR_tracts/OR_WF_tracts_data_",HMSday,".RDS"))
     
     # clear temp folder
     
@@ -317,25 +203,32 @@ intersectHMSdirect <- function(HMSday){
 }
 
 
-
 ############### run multiple intersects  for archived data #########
 ###############   download here https://www.census.gov/geo/maps-data/data/tiger-kml.html ##########
 
 library(data.table)
 
-stateKey <- fread("~/GitHub/WFSmokeExp/stateKey.csv") %>% 
-  mutate(charST = ifelse(stFIPS<10, paste0("0",stFIPS), as.character(stFIPS)))
-
-BG_list <- lapply(stateKey$charST, function(x) paste0("https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG",x,".txt"))
-
-tableList <- lapply(BG_list, fread)
-
-foo <- st_as_sf(rbindlist(tableList), coords = c("LONGITUDE", "LATITUDE"), crs = 4326) 
-
-
-CAzips <- st_read("~/Wildfires/data/spatial/") %>% st_transform(crs = 4326) %>%
-  mutate(zip = sapply(str_split(Description, pattern = "[<>]+"), "[[", 18)) %>% select(zip, geometry)
+# stateKey <- fread("~/GitHub/WFSmokeExp/stateKey.csv") %>% 
+#   mutate(charST = ifelse(stFIPS<10, paste0("0",stFIPS), as.character(stFIPS)))
 # 
+# BG_list <- lapply(stateKey$charST, function(x) paste0("https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG",x,".txt"))
+# 
+# tableList <- lapply(BG_list, fread)
+
+tableList <- fread("https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG.txt")
+
+popCentroidsBG <- st_as_sf(tableList, coords = c("LONGITUDE", "LATITUDE"), crs = 4326) 
+ 
+
+
+############# combine with zips for EHIB 
+
+# CAzips <- st_read("~/GitHub/WFSmokeExp_US/cb_2016_us_zcta510_500k.kml") %>% st_transform(crs = 4326) %>%
+#     mutate(zip = sapply(str_split(Description, pattern = "[<>]+"), "[[", 18)) %>% select(zip, geometry)
+
+ # CAzipsBG <-st_intersection({popCentroidsBG %>% filter(STATEFP == 6)}, CAzips)
+
+
 # 
 # CAtractFile <-  st_read(dsn = "~/GitHub/WFSmokeExp/SmokeExposures/tractsSM.GeoJSON", stringsAsFactors = F) %>%
 #   st_transform(crs = 4326) %>%
@@ -355,21 +248,13 @@ CAzips <- st_read("~/Wildfires/data/spatial/") %>% st_transform(crs = 4326) %>%
 #          CountyFIPS = sapply(str_split(Description, pattern = "[<>]+"), "[[", 62))  %>%
 #   rename(geoIDer = CountyFIPS)
 
-# UStractFile <- st_read(dsn = "~/GitHub/WFSmokeExp_US/cb_2017_us_county_20m.kml", stringsAsFactors = F) %>% st_transform(crs = 4326) %>%
-#   mutate(County = sapply(str_split(Name, pattern = "[<>]+"), "[[",4),
-#          CountyFIPS = sapply(str_split(Description, pattern = "[<>]+"), "[[", 62))  %>%
-#   rename(geoIDer = CountyFIPS)
 
-
-
-
-dateListarchive <- format(seq(as.Date("2016/12/22"),as.Date("2018/03/02"), by = "day"),"%Y%m%d")
-mclapply(dateListarchive, FUN = intersectHMSarchive)
-
+# dateListarchive <- format(seq(as.Date("2010/05/30"),as.Date("2010/05/30"), by = "day"),"%Y%m%d")
+# mclapply(dateListarchive, FUN = intersectHMSarchive)
 
 # check this website to see the most recent date available http://satepsanone.nesdis.noaa.gov/pub/FIRE/HMS/GIS/ARCHIVE/
 
-dateListrecent <- format(seq(as.Date("2018/08/27"),as.Date("2018/09/17"), by = "day"),"%Y%m%d")
+dateListrecent <- format(seq(as.Date("2018/09/23"),as.Date("2018/09/30"), by = "day"),"%Y%m%d")
 mclapply(dateListrecent, FUN = intersectHMSrecent)
 
 ########### function to combine ############
@@ -377,29 +262,247 @@ mclapply(dateListrecent, FUN = intersectHMSrecent)
 # when combining several 
 
 library(data.table)
-library(ggplot2)
 library(scales)
 library(lubridate)
 
-dateList <- format(seq(as.Date("2010/06/01"),as.Date("2018/09/115"), by = "day"),"%Y%m%d")
+directory <- "~/data/US_BG/RDS"
+allBGs <- tableList[, geoIDer := paste0(STATEFP,"_",COUNTYFP,"_",TRACTCE,"_",BLKGRPCE)]
 
-area <- "CA" # enter 'County' or 'CA' 
 
-if(area == "US"){
-  directory <- "~/data/US_counties/"
-  spatialFile <- UScountyFile
-} else if(area == "CA"){
-  directory <- "~/data/CA_tracts/"
-  spatialFile <- CAtractFile
-} else if(area == "WA"){
-  directory <- "~/data/WA_tracts/"
-  spatialFile <- WAtractFile
-} else if(area == "OR"){
-  directory <- "~/data/OR_tracts/"
-  spatialFile <- ORtractFile
-} else{
-  return()
+convert <- function(RDSfile){
+  
+  fwrite(readRDS(RDSfile), paste0("~/data/US_BG/CSV/", sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(RDSfile)),".csv"))
+  
 }
+
+mclapply(list.files(directory,  full.names = T), convert)
+
+
+smokeFile <- rbindlist(mclapply(list.files("~/data/US_BG/CSV/",full.names = T), fread), fill = T)
+
+setkey(smokeFile, date, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE) 
+
+smokeFile <-copy(smokeFile)
+
+
+crunchDayCA <- function(Year){
+  
+  smokeFile <-
+    rbindlist(mclapply(list.files(
+      directory,
+      full.names = T,
+      pattern = paste0("BG", Year)
+    ), readRDS), fill = T) %>% setkey(date, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE) %>% replace_na(list(
+      light = 0,
+      medium = 0,
+      heavy = 0
+    )) %>%
+    .[, maxSmoke := factor(ifelse(heavy == 1, "heavy",
+                                    ifelse(medium == 1, "medium", 
+                                           ifelse(light ==1, "light", "none"))), levels=c("none","light","medium","heavy"))] %>%
+    .[STATEFP == 6]
+    
+  
+}
+
+CA <- rbindlist(lapply(X = c(2011:2018), FUN = crunchDayCA)) %>% merge(CAzipsBG)
+
+
+  CA[date > "20141231"]%>%
+    .[order(date)]
+  
+  
+
+# Year= 2010
+
+crunchYearState<- function(Year){
+  
+  smokeFile <-
+    rbindlist(mclapply(list.files(
+      directory,
+      full.names = T,
+      pattern = paste0("BG", Year)
+    ), readRDS), fill = T) %>% setkey(date, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE) %>% replace_na(list(
+      light = 0,
+      medium = 0,
+      heavy = 0
+    )) 
+  
+  
+    smokeFile[,c("lightPD", "mediumPD","heavyPD") := list(light * POPULATION,
+                                                  medium * POPULATION,
+                                                  heavy * POPULATION)]     
+    
+    return({
+      smokeFile[, list(sum(lightPD),
+                       sum(mediumPD),
+                       sum(heavyPD)), by = "STATEFP"] %>% mutate(Year = Year)
+    })
+
+}
+
+crunchYearCounty<- function(Year){
+  
+  smokeFile <-
+    rbindlist(mclapply(list.files(
+      directory,
+      full.names = T,
+      pattern = paste0("BG", Year)
+    ), readRDS), fill = T) %>% setkey(date, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE) %>% replace_na(list(
+      light = 0,
+      medium = 0,
+      heavy = 0
+    )) 
+  
+  
+  smokeFile[,c("lightPD", "mediumPD","heavyPD") := list(light * POPULATION,
+                                                        medium * POPULATION,
+                                                        heavy * POPULATION)]     
+  
+  return({
+    smokeFile[, list(sum(),
+                     sum(lightPD),
+                     sum(mediumPD),
+                     sum(heavyPD)), by = c("STATEFP", "COUNTYFP")] %>% mutate(Year = Year)
+  })
+  
+}
+
+crunchYearBG<- function(Year){
+  
+  smokeFile <-
+    rbindlist(mclapply(list.files(
+      directory,
+      full.names = T,
+      pattern = paste0("BG", Year)
+    ), readRDS), fill = T) %>% setkey(date, STATEFP, COUNTYFP, TRACTCE, BLKGRPCE) %>% replace_na(list(
+      light = 0,
+      medium = 0,
+      heavy = 0
+    )) 
+  
+  
+  smokeFile[,c("lightPD", "mediumPD","heavyPD") := list(light * POPULATION,
+                                                        medium * POPULATION,
+                                                        heavy * POPULATION)]     
+  
+  return({
+    smokeFile[, list(mean(POPULATION),
+                     sum(lightPD),
+                     sum(mediumPD),
+                     sum(heavyPD)), by = c("STATEFP", "COUNTYFP", "TRACTCE", "BLKGRPCE")] %>% mutate(Year = Year)
+  })
+  
+}
+
+# look at heavy smoke by state and year
+
+annualPersonDaysState <- rbindlist(lapply(X = c(2011:2018), FUN = crunchYearState))
+names(annualPersonDaysState) <- c("stFIPS","LightPDs","MediumPDs","HeavyPDs","Year")
+annualPersonDaysState %>% merge(stateKey) %>% write.csv("~/tempZip/stateAnnualPDs.csv", row.names = F)
+
+
+annualPersonDays %>% merge(stateKey) %>% 
+  gather(LightPDs,MediumPDs,HeavyPDs,key = smoke, value = PersonDays) %>%
+  filter(smoke == "HeavyPDs") %>%
+  ggplot() + 
+  geom_bar(aes(x=Year, y=PersonDays, fill= smoke), stat="identity",position="dodge") + facet_wrap(~ stateName)
+
+westCoast <- c("CA","OR","WA") 
+midWest <- c("MI","WI","MN","IL","OH") 
+
+annualPersonDays %>% merge(stateKey) %>% 
+  gather(LightPDs,MediumPDs,HeavyPDs,key = smoke, value = PersonDays) %>%
+  filter(ST %in% westCoast) %>%
+  mutate(smoke = factor(smoke, levels=c("LightPDs","MediumPDs","HeavyPDs")))%>%
+  ggplot() + 
+  geom_bar(aes(x=Year, y=PersonDays, fill= smoke), stat="identity",position="dodge") + facet_grid( smoke ~stateName , scales = "free_y")
+
+
+
+
+# look at heavy smoke by County and year
+
+annualPersonDaysCounty <- rbindlist(lapply(X = c(2011:2018), FUN = crunchYearCounty)) 
+annualPersonDaysCounty <- annualPersonDaysCounty %>% 
+  mutate(geoIDer = paste0(
+    {
+      ifelse(STATEFP < 10, paste0("0",STATEFP),STATEFP)
+    },
+    {
+      ifelse(COUNTYFP < 10, paste0("00",COUNTYFP), 
+             ifelse(COUNTYFP < 100, paste0("0",COUNTYFP),COUNTYFP))
+      
+    }
+  ))
+names(annualPersonDaysCounty) <- c("stFIPS", "ctFIPS","LightPDs","MediumPDs","HeavyPDs","Year", "geoIDer")
+
+UScountyFile <- st_read(dsn = "~/GitHub/WFSmokeExp_US/cb_2017_us_county_20m.kml", stringsAsFactors = F) %>% st_transform(crs = 4326) %>%
+  mutate(County = sapply(str_split(Name, pattern = "[<>]+"), "[[",4),
+         CountyFIPS = sapply(str_split(Description, pattern = "[<>]+"), "[[", 62))  %>%
+  rename(geoIDer = CountyFIPS) %>%
+  merge(annualPersonDaysCounty) %>%  
+  filter(stFIPS == 6) %>% 
+  st_crop(c(xmin=-145, xmax=90, ymin=20, ymax=50)) # lower 48 bounds
+
+
+UScountyFile %>%
+  gather(LightPDs,MediumPDs,HeavyPDs,key = smoke, value = PersonDays) %>%
+  mutate(smoke = factor(smoke, levels=c("LightPDs","MediumPDs","HeavyPDs")))%>%
+  filter(smoke == "HeavyPDs") %>%
+ggplot() + geom_sf(aes(fill = PersonDays)) +
+  scale_fill_distiller(palette = "Spectral") + 
+  facet_grid(smoke~Year)
+
+
+
+# look at heavy smoke by County and year
+
+annualPersonDaysBG <- rbindlist(lapply(X = c(2011:2018), FUN = crunchYearBG))
+names(annualPersonDaysBG) <- c("STATEFP", "COUNTYFP", "TRACTCE", "BLKGRPCE","POPULATION", "LightPDs","MediumPDs","HeavyPDs","Year")
+
+foo <- annualPersonDaysBG[, geoIDer := paste0(STATEFP,"_",COUNTYFP,"_",TRACTCE,"_",BLKGRPCE)] # %>%
+  .[STATEFP ==6]
+
+
+#######  function to create the cummulative percentages for each year
+cumPCT <- function(year){
+  
+  return({foo[Year==year] %>%
+      .[order(-HeavyPDs)] %>%
+      .[,CUMpop:= cumsum(POPULATION)/sum(POPULATION)] %>%
+      .[,CUMexp:= cumsum(HeavyPDs)/sum(HeavyPDs)] })
+
+}
+
+test <- rbindlist(lapply(X = c(2011:2018), FUN = cumPCT))
+
+  
+test %>%  
+  ggplot() + 
+  geom_line(aes(x=CUMpop, y=CUMpop), color="black", linetype="dashed") +
+  geom_line(aes(x=CUMpop, y=CUMexp, color=factor(Year), group= Year), size=2) + 
+  scale_x_continuous(name="Fraction of US Population", limits=c(0, .75)) + 
+  ylab("Fraction of the Annual Heavy WF Smoke Exposure")
+
+  test[,sum(HeavyPDs)/1000000, by="Year"] %>% 
+    ggplot() + geom_bar(aes(x=Year, y=V1), stat= "identity")
+
+
+
+
+
+
+
+dateList <- format(seq(as.Date(paste0(Year,"/01/01")),as.Date(paste0(Year,"/12/31")), by = "day"),"%Y%m%d")
+
+
+
+baseGrid <- data.table(expand.grid(date = dateList, geoIDer = unique(allBGs$geoIDer)), key = c("date","STATEFP","COUNTYFP","TRACTCE","BLKGRPCE"))
+
+return(baseGrid)
+
+
 
 BG <- rbindlist(tableList) %>% .[, geoIDer := paste0(STATEFP,"_",COUNTYFP,"_",TRACTCE,"_",BLKGRPCE)]
 
@@ -536,11 +639,12 @@ mapData <-   master[, "Year" := year(as.Date(date, "%Y%m%d"))] %>%
 
 
  fart <- UScountyFile %>% left_join(mapData) %>%  
-   filter(stFIPS < 57 & stFIPS != 15 ) %>% st_crop(c(xmin=-125, xmax=-116, ymin=41.5, ymax=46.5))
+   filter(stFIPS < 57 & stFIPS != 15 ) %>% st_crop(c(xmin=-125, xmax=-116, ymin=41.5, ymax=46.5)) # crop to lower 48
 
    
    
-   st_crop(c(xmin=-145, xmax=90, ymin=20, ymax=50))
+   st_crop(c(xmin=-145, xmax=90, ymin=20, ymax=50)) # crop to OR
+   st_crop(c(xmin=-145, xmax=90, ymin=20, ymax=50)) # crop to CA
 
  breaks <- c(0,10,20,30,40, 50,60)
  
